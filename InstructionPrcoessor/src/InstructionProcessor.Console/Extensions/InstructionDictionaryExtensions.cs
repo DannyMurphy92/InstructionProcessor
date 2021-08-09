4683f19d056e909fc4564f09910ce1cfbf84e05a
Danny Murphy <Danny.Murphy@decision.tech>
2021-08-09 18:16:56 +0100

    Added instruction dictionary add extension method

---
+++ InstructionPrcoessor/src/InstructionProcessor.Console/Extensions/InstructionDictionaryExtensions.cs
@@ -0,0 +1,48 @@
+﻿using InstructionProcessor.Console.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace InstructionProcessor.Console.Extensions
+{
+    public static class InstructionDictionaryExtensions
+    {
+        private const string DefaultPattern = @"^(\d+):\s+(\w+)\s+((\d+\s*)+)$";
+        private const int DefaultLabelGroupIx = 1;
+        private const int DefaultActionGroupIx = 2;
+        private const int DefaultValueGroupIx = 3;
+
+        public static void AddInstruction(
+            this IDictionary<int, Instruction> dict,
+            string input,
+            string instructionPattern = DefaultPattern,
+            int labelGrounpIx = DefaultLabelGroupIx,
+            int actionGrounpIx = DefaultActionGroupIx,
+            int valueGrounpIx = DefaultValueGroupIx
+            )
+        {
+            var regex = new Regex(instructionPattern, RegexOptions.IgnoreCase);
+
+            var match = regex.Match(input);
+
+            if (!match.Success)
+            {
+                throw new ArgumentException($"\"{input}\" does not match the supplied pattern \"{instructionPattern}\"");
+            }
+
+            var label = int.Parse(match.Groups[labelGrounpIx].Value);
+            var action = match.Groups[actionGrounpIx].Value.ToUpper();
+            var values = match.Groups[valueGrounpIx].Value.Split(' ').Select(val => int.Parse(val));
+
+            var instruction = new Instruction
+            {
+                Action = action,
+                Values = values
+            };
+
+            dict.Add(label, instruction);
+        }
+    }
+}
---
+++ InstructionPrcoessor/tests/InstructionProcess.Console.UnitTests/Extensions/InstructionDictionaryExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using InstructionProcessor.Console.Extensions;
+using InstructionProcessor.Console.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstructionProcess.Console.UnitTests.Extensions
+{
+    [TestFixture]
+    public class InstructionDictionaryExtensionsTests
+    {
+        [Test]
+        public void AddInstruction_GivenInstructionInDefaultFormat_AddsCorrectly()
+        {
+            var sut = new Dictionary<int, Instruction>();
+            sut.AddInstruction("123: Add 100 200");
+
+            var instruction = sut[123];
+            Assert.AreEqual("ADD", instruction.Action);
+            Assert.AreEqual(new[] { 100, 200 }, instruction.Values);
+        }
+
+        [Test]
+        public void AddInstruction_GivenInstructionInDifferentFormatWithMatchingRegexAndIndexes_AddsCorrectly()
+        {
+            var sut = new Dictionary<int, Instruction>();
+            sut.AddInstruction("Add 123: 100 200", @"^(\w+)\s+(\d+):\s+((\d+\s*)+)$", 2, 1, 3);
+
+            var instruction = sut[123];
+            Assert.AreEqual("ADD", instruction.Action);
+            Assert.AreEqual(new[] { 100, 200 }, instruction.Values);
+        }
+
+        [Test]
+        public void AddInstruction_InputThatDoesntMatchPattern_ThrowsException()
+        {
+            var sut = new Dictionary<int, Instruction>();
+            var ex = Assert.Throws<ArgumentException>(() => sut.AddInstruction("not a valid input", "^\\d*$"));
+            Assert.AreEqual("\"not a valid input\" does not match the supplied pattern \"^\\d*$\"", ex.Message);
+        }
+    }
+}
